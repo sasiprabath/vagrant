713e027c4fab1a9f3c5981f0817a05fe61430dd4
sasiprabath <sasi.prabath@gmail.com>
2016-06-18 05:29:12 +0000

    shell scripting

---
+++ form.sh
@@ -0,0 +1,33 @@
+#form
+
+echo " Enter Fisrt Name"
+read name
+echo " Enter Second Name"
+read second
+echo "Enter Your Address"
+read add
+echo "Enter Your Age"
+read age
+echo "Enter Your DOB"
+read dob
+
+
+clear
+cal
+
+echo "================================"
+echo "USER FORM"
+echo "================================"
+
+echo "First name    : $name"
+
+echo "Second Name   : $second"
+
+echo "Address       : $add"
+
+echo "Your age is   : $age"
+
+echo "Date of Birth : $dob"
+
+
+echo "=============================="
